6521bb7deed5a568f3b704cc1be622dfdb9b2052
Mark Retallack <mark.retallack@yunextraffic.com>
2024-01-15 08:21:21 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kafka_rest",
+    version="0.0.1",
+    author="shashi1991, mark.retallack@yunextraffic.com",
+    author_email="mark.retallack@yunextraffic.com",
+    description="fork of nlp-kafka-rest-api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/nlp-kafka-rest-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
